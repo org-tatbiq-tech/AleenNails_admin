c4e008ecbb5d4a096d41f1a5456d0bf04b634115
Ahmad <ahmadmnaa.b@gmail.com>
2022-04-13 15:11:50 +0300

    a lot of files

--- lib/screens/home/appointments/new_appointment.dart
+++ lib/screens/home/appointments/new_appointment.dart
@@ -1,10 +1,13 @@
 import 'package:appointments/utils/data_types.dart';
 import 'package:appointments/utils/date.dart';
 import 'package:appointments/utils/layout.dart';
+import 'package:appointments/widget/accrodion/accordion.dart';
+import 'package:appointments/widget/accrodion/accordion_section.dart';
+import 'package:appointments/widget/accrodion/controllers.dart';
 import 'package:appointments/widget/contact_card.dart';
 import 'package:appointments/widget/custom_app_bar.dart';
-import 'package:appointments/widget/custom_text_button.dart';
 import 'package:appointments/widget/expandable_calendar.dart';
+import 'package:appointments/widget/slot_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -20,7 +23,7 @@ class _NewAppointmentState extends State<NewAppointment> {
   late final ValueNotifier<List<CalendarEvent>> _selectedEvents;
   CalendarFormat _calendarFormat = CalendarFormat.week;
   DateTime _focusedDay = kToday;
-  Contact? selectedContact = Contact('name', 'phone', 'address');
+  Contact? selectedContact;
   DateTime? _selectedDay;
   List<String> slots = [
     '11:00',
@@ -60,87 +63,167 @@ class _NewAppointmentState extends State<NewAppointment> {
     }
   }
 
-  Widget _renderSlot(BuildContext context, item) {
-    return SizedBox(
-      width: rSize(80),
-      child: Card(
-        // margin: EdgeInsets.fromLTRB(0, 0, 0, rSize(20)),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            rSize(15),
-          ),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              item,
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    _renderClient() {
-      return Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Client',
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                      fontSize: rSize(18),
-                    ),
+    _renderAccordionSection({
+      required Widget header,
+      required Widget content,
+      Widget? leftIcon,
+      int index = 0,
+      bool isDisabled = false,
+      bool isOpen = false,
+    }) {
+      return AccordionSection(
+        index: index,
+        isOpen: isOpen,
+        leftIcon: leftIcon,
+        header: header,
+        content: content,
+        isDisabled: isDisabled,
+      );
+    }
+
+    _renderSlotsHeader() {
+      return Text(
+        'Available Slots'.toUpperCase(),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        style: Theme.of(context).textTheme.headline1?.copyWith(
+              fontSize: rSize(18),
+              color: Theme.of(context).colorScheme.primary,
+            ),
+      );
+    }
+
+    _renderSlotsBody(List<String> item) {
+      return ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: rSize(60),
+        ),
+        child: Center(
+          child: SizedBox(
+            height: rSize(50),
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.symmetric(
+                horizontal: rSize(10),
               ),
-              CustomTextButton(
-                  customTextButtonProps: CustomTextButtonProps(
-                      onTap: () => {},
-                      textStyle:
-                          Theme.of(context).textTheme.bodyText2?.copyWith(
-                                fontSize: rSize(18),
-                                color: Theme.of(context).colorScheme.primary,
-                                decoration: TextDecoration.underline,
-                                decorationStyle: TextDecorationStyle.solid,
-                                decorationThickness: rSize(10),
-                              ),
-                      text: selectedContact != null
-                          ? 'Change Contact'
-                          : 'Select Contact'))
-            ],
+              itemCount: slots.length,
+              itemBuilder: (context, index) {
+                return SlotCard(
+                  item: slots[index],
+                );
+              },
+            ),
           ),
-          SizedBox(
-            height: rSize(20),
-          ),
-          selectedContact != null
-              ? ContactCard(
-                  contactCardProps: ContactCardProps(
-                    withNavigation: false,
-                    contactDetails: selectedContact!,
-                  ),
-                )
-              : Center(
-                  child: Text(
-                    'No Contact Selected',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
+        ),
+      );
+    }
+
+    _renderServicesHeader() {
+      return Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'choose your services'.toUpperCase(),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.headline1?.copyWith(
+                  fontSize: rSize(18),
+                  color: Theme.of(context).colorScheme.primary,
                 ),
+          ),
+          // EaseInAnimation(
+          //   onTap: () => {},
+          //   child: IconTheme(
+          //     data: Theme.of(context).primaryIconTheme,
+          //     child: Icon(
+          //       FontAwesomeIcons.plus,
+          //       size: rSize(20),
+          //     ),
+          //   ),
+          // ),
         ],
+      );
+    }
+
+    _renderServicesBody() {
+      return ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: rSize(100),
+        ),
+        child: selectedContact != null
+            ? ContactCard(
+                contactCardProps: ContactCardProps(
+                  withNavigation: false,
+                  contactDetails: selectedContact!,
+                ),
+              )
+            : Center(
+                child: Text(
+                  'No Services Selected',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ),
+      );
+    }
+
+    _renderClientHeader() {
+      return Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Choose your client'.toUpperCase(),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.headline1?.copyWith(
+                  fontSize: rSize(18),
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+          // EaseInAnimation(
+          //   onTap: () => {},
+          //   child: IconTheme(
+          //     data: Theme.of(context).primaryIconTheme,
+          //     child: Icon(
+          //       selectedContact != null
+          //           ? FontAwesomeIcons.userPen
+          //           : FontAwesomeIcons.userPlus,
+          //       size: rSize(20),
+          //     ),
+          //   ),
+          // ),
+        ],
+      );
+    }
+
+    _renderClientBody() {
+      return ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: rSize(100),
+        ),
+        child: selectedContact != null
+            ? ContactCard(
+                contactCardProps: ContactCardProps(
+                  withNavigation: false,
+                  contactDetails: selectedContact!,
+                ),
+              )
+            : Center(
+                child: Text(
+                  'No Client Selected',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ),
       );
     }
 
@@ -179,37 +262,65 @@ class _NewAppointmentState extends State<NewAppointment> {
               },
             ),
           ),
-          SingleChildScrollView(
-            padding: EdgeInsets.symmetric(
-              horizontal: rSize(20),
-              vertical: rSize(30),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _renderClient(),
-                SizedBox(
-                  height: rSize(20),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: rSize(15), vertical: rSize(20)),
+              child: Accordion(
+                contentBorderWidth: 0,
+                maxOpenSections: 1,
+                contentBorderColor: Theme.of(context).colorScheme.primary,
+                contentBackgroundColor:
+                    Theme.of(context).colorScheme.background,
+                headerBackgroundColor:
+                    Theme.of(context).colorScheme.onBackground,
+                headerBackgroundColorOpened:
+                    Theme.of(context).colorScheme.onBackground,
+                contentHorizontalPadding: rSize(0),
+                headerPadding: EdgeInsets.symmetric(
+                  vertical: rSize(10),
+                  horizontal: rSize(10),
                 ),
-                SizedBox(
-                  height: rSize(50),
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    // padding: EdgeInsets.only(
-                    //   top: rSize(20),
-                    //   left: rSize(20),
-                    //   right: rSize(20),
-                    // ),
-                    itemCount: slots.length,
-                    itemBuilder: (context, index) {
-                      return _renderSlot(context, slots[index]);
-                    },
+                sectionOpeningHapticFeedback: SectionHapticFeedback.heavy,
+                sectionClosingHapticFeedback: SectionHapticFeedback.light,
+                rightIcon: IconTheme(
+                  data: Theme.of(context).primaryIconTheme,
+                  child: Icon(
+                    FontAwesomeIcons.chevronDown,
+                    size: rSize(16),
                   ),
                 ),
-              ],
+                children: [
+                  _renderAccordionSection(
+                    index: 0,
+                    header: _renderClientHeader(),
+                    content: _renderClientBody(),
+                    leftIcon: IconTheme(
+                      data: Theme.of(context).primaryIconTheme,
+                      child: Icon(
+                        Icons.person,
+                        size: rSize(26),
+                      ),
+                    ),
+                    isDisabled: false,
+                    isOpen: false,
+                  ),
+                  _renderAccordionSection(
+                    index: 1,
+                    header: _renderServicesHeader(),
+                    content: _renderServicesBody(),
+                    isDisabled: false,
+                    isOpen: false,
+                  ),
+                  _renderAccordionSection(
+                    index: 2,
+                    header: _renderSlotsHeader(),
+                    content: _renderSlotsBody(slots),
+                    isDisabled: false,
+                    isOpen: true,
+                  )
+                ],
+              ),
             ),
           ),
         ],
---
+++ lib/widget/accrodion/accordion_section.dart
@@ -0,0 +1,264 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:appointments/utils/layout.dart';
+import 'package:appointments/widget/ease_in_animation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
+import 'package:appointments/widget/accrodion/accordion.dart';
+import 'package:appointments/widget/accrodion/controllers.dart';
+
+class AccordionSection extends StatelessWidget with CommonParams {
+  final SectionController sectionCtrl = SectionController();
+  late final UniqueKey uniqueKey;
+  late final int index;
+  final listCtrl = Get.put(ListController());
+  final bool isOpen;
+  final bool isDisabled;
+
+  /// The text to be displayed in the header
+  final Widget header;
+
+  /// The widget to be displayed as the content of the section when open
+  final Widget content;
+
+  AccordionSection({
+    Key? key,
+    this.index = 0,
+    this.isOpen = false,
+    this.isDisabled = false,
+    required this.header,
+    required this.content,
+    Color? headerBackgroundColor,
+    Color? headerBackgroundColorOpened,
+    double? headerBorderRadius,
+    EdgeInsets? headerPadding,
+    Widget? leftIcon,
+    Widget? rightIcon,
+    bool? flipRightIconIfOpen = true,
+    Color? contentBackgroundColor,
+    Color? contentBorderColor,
+    double? contentBorderWidth,
+    double? contentBorderRadius,
+    double? contentHorizontalPadding,
+    double? contentVerticalPadding,
+    double? paddingBetweenOpenSections,
+    double? paddingBetweenClosedSections,
+    ScrollIntoViewOfItems? scrollIntoViewOfItems,
+    SectionHapticFeedback? sectionOpeningHapticFeedback,
+    SectionHapticFeedback? sectionClosingHapticFeedback,
+  }) : super(key: key) {
+    uniqueKey = listCtrl.keys.elementAt(index);
+    sectionCtrl.isSectionOpen.value = listCtrl.openSections.contains(uniqueKey);
+
+    this.headerBackgroundColor = headerBackgroundColor;
+    this.headerBackgroundColorOpened =
+        headerBackgroundColorOpened ?? headerBackgroundColor;
+    this.headerBorderRadius = headerBorderRadius;
+    this.headerPadding = headerPadding;
+    this.leftIcon = leftIcon;
+    this.rightIcon = rightIcon;
+    this.flipRightIconIfOpen?.value = flipRightIconIfOpen ?? true;
+    this.contentBackgroundColor = contentBackgroundColor;
+    this.contentBorderColor = contentBorderColor;
+    this.contentBorderWidth = contentBorderWidth ?? rSize(1);
+    this.contentBorderRadius = contentBorderRadius ?? rSize(15);
+    this.contentHorizontalPadding = contentHorizontalPadding ?? rSize(15);
+    this.contentVerticalPadding = contentVerticalPadding ?? rSize(15);
+    this.paddingBetweenOpenSections = paddingBetweenOpenSections ?? rSize(15);
+    this.paddingBetweenClosedSections =
+        paddingBetweenClosedSections ?? rSize(15);
+    this.scrollIntoViewOfItems =
+        scrollIntoViewOfItems ?? ScrollIntoViewOfItems.fast;
+    this.sectionOpeningHapticFeedback = sectionOpeningHapticFeedback;
+    this.sectionClosingHapticFeedback = sectionClosingHapticFeedback;
+
+    listCtrl.controllerIsOpen.stream.asBroadcastStream().listen((data) {
+      sectionCtrl.isSectionOpen.value = listCtrl.openSections.contains(key);
+    });
+  }
+
+  /// getter to flip the widget vertically (Icon by default)
+  /// on the right of this section header to visually indicate
+  /// if this section is open or closed
+  get _flipQuarterTurns =>
+      flipRightIconIfOpen?.value == true ? (_isOpen ? 2 : 0) : 0;
+
+  /// getter indication the open or closed status of this section
+  get _isOpen {
+    final open = sectionCtrl.isSectionOpen.value;
+
+    Timer(
+      sectionCtrl.firstRun
+          ? (listCtrl.initialOpeningSequenceDelay + min(index * 200, 1000))
+              .milliseconds
+          : 0.seconds,
+      () {
+        if (Accordion.sectionAnimation) {
+          sectionCtrl.controller
+              .fling(velocity: open ? 1 : -1, springDescription: springFast);
+        } else {
+          sectionCtrl.controller.value = open ? 1 : 0;
+        }
+        sectionCtrl.firstRun = false;
+      },
+    );
+
+    return open;
+  }
+
+  /// play haptic feedback when opening/closing sections
+  _playHapticFeedback(bool opening) {
+    final feedback =
+        opening ? sectionOpeningHapticFeedback : sectionClosingHapticFeedback;
+
+    switch (feedback) {
+      case SectionHapticFeedback.light:
+        HapticFeedback.lightImpact();
+        break;
+      case SectionHapticFeedback.medium:
+        HapticFeedback.mediumImpact();
+        break;
+      case SectionHapticFeedback.heavy:
+        HapticFeedback.heavyImpact();
+        break;
+      case SectionHapticFeedback.selection:
+        HapticFeedback.selectionClick();
+        break;
+      case SectionHapticFeedback.vibrate:
+        HapticFeedback.vibrate();
+        break;
+      default:
+    }
+  }
+
+  @override
+  build(context) {
+    final _borderRadius = headerBorderRadius ?? 10;
+
+    return Obx(
+      () => Column(
+        key: uniqueKey,
+        children: [
+          EaseInAnimation(
+            isDisabled: isDisabled,
+            beginAnimation: 0.99,
+            onTap: () {
+              listCtrl.updateSections(uniqueKey);
+              _playHapticFeedback(_isOpen);
+
+              if (_isOpen &&
+                  scrollIntoViewOfItems != ScrollIntoViewOfItems.none &&
+                  listCtrl.controller.hasClients) {
+                Timer(
+                  250.milliseconds,
+                  () {
+                    listCtrl.controller.cancelAllHighlights();
+                    listCtrl.controller.scrollToIndex(index,
+                        preferPosition: AutoScrollPosition.middle,
+                        duration:
+                            (scrollIntoViewOfItems == ScrollIntoViewOfItems.fast
+                                    ? .5
+                                    : 1)
+                                .seconds);
+                  },
+                );
+              }
+            },
+            child: AnimatedContainer(
+              duration: Accordion.sectionAnimation
+                  ? 750.milliseconds
+                  : 0.milliseconds,
+              curve: Curves.easeOut,
+              alignment: Alignment.center,
+              padding: headerPadding,
+              decoration: BoxDecoration(
+                color: (_isOpen
+                        ? headerBackgroundColorOpened
+                        : headerBackgroundColor) ??
+                    Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(_borderRadius),
+                  bottom: Radius.circular(_isOpen ? 0 : _borderRadius),
+                ),
+              ),
+              child: Row(
+                children: [
+                  if (leftIcon != null) leftIcon!,
+                  Expanded(
+                    flex: 10,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: leftIcon == null ? 0 : 15),
+                      child: header,
+                    ),
+                  ),
+                  if (rightIcon != null)
+                    RotatedBox(
+                        quarterTurns: _flipQuarterTurns, child: rightIcon!),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                bottom: _isOpen
+                    ? paddingBetweenOpenSections!
+                    : paddingBetweenClosedSections!),
+            child: SizeTransition(
+              sizeFactor: sectionCtrl.controller,
+              child: ScaleTransition(
+                scale: sectionCtrl.controller,
+                child: Center(
+                  child: Container(
+                    // clipBehavior: Clip.antiAliasWithSaveLayer,
+                    decoration: BoxDecoration(
+                      color:
+                          contentBorderColor ?? Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.vertical(
+                          bottom: Radius.circular(contentBorderRadius!)),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(
+                        contentBorderWidth ?? rSize(1),
+                        0,
+                        contentBorderWidth ?? rSize(1),
+                        contentBorderWidth ?? rSize(1),
+                      ),
+                      child: Container(
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.vertical(
+                                bottom: Radius.circular(
+                                    contentBorderRadius! / 1.02))),
+                        child: Container(
+                          clipBehavior: Clip.antiAlias,
+                          decoration: BoxDecoration(
+                              color: contentBackgroundColor,
+                              borderRadius: BorderRadius.vertical(
+                                  bottom: Radius.circular(
+                                      contentBorderRadius! / 1.02))),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: contentHorizontalPadding!,
+                              vertical: contentVerticalPadding!,
+                            ),
+                            child: Center(
+                              child: content,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
