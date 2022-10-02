75475df4c71a5aa77716928a91db54a6008522ea
S.Isa <s.isa.developments@gmail.com>
2022-10-02 13:20:26 +0300

    cloud function

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,8 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 // Macros
-const appointmentsCollection = "stores";
-const clientsCollection = "stores";
+const appointmentsCollection = "appointments";
+const clientsCollection = "clients";
 
 // Creating new appointment function listener, one new appointment added, need to update client's appointment list
 
@@ -13,10 +13,34 @@ exports.newAppointment = functions.firestore.
     onCreate((snap, context) => {
 
         // Getting details of the new document
-        const appointmentId = context.params.packageId;
+        const appointmentId = context.params.appointmentId;
         const newAppointmentData = snap.data();
 
         const clientDocID = newAppointmentData.clientDocID;
         const client = admin.firestore().collection(clientsCollection).doc(clientDocID);
+        let myAppointment = {
+            appointmentId: { id: appointmentId}
 
-    }
+        }
+        return client.update({
+                   appointments: myAppointment
+       });
+//        return client.get().then(clientDoc => {
+//            clientDoc
+//        }
+
+         // Setting notification content - must send package_id
+//        const notificationContent = {
+//            notification: {
+//                title: title,
+//                body: msg,
+//                sound: 'default'
+//            },
+//            data: {
+//                package_id: packageId
+//            }
+//        };
+//
+//        return admin.messaging().sendToDevice(store.data()["tokens"], notificationContent);
+
+    })
--- lib/screens/home/clients/clientSelection.dart
+++ lib/screens/home/clients/clientSelection.dart
@@ -1,4 +1,4 @@
-import 'package:appointments/providers/app_data.dart';
+import 'package:appointments/providers/clients_mgr.dart';
 import 'package:appointments/screens/home/clients/client.dart';
 import 'package:appointments/widget/client_card.dart';
 import 'package:common_widgets/custom_app_bar.dart';
@@ -23,8 +23,8 @@ class ClientSelection extends StatefulWidget {
 class ClientSelectionState extends State<ClientSelection> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppData>(
-      builder: (context, appData, _) {
+    return Consumer<ClientsMgr>(
+      builder: (context, clientsMgr, _) {
         return Scaffold(
           appBar: CustomAppBar(
             customAppBarProps: CustomAppBarProps(
@@ -62,15 +62,15 @@ class ClientSelectionState extends State<ClientSelection> {
                     vertical: rSize(20),
                     horizontal: rSize(20),
                   ),
-                  itemCount: appData.filteredContacts.length,
+                  itemCount: clientsMgr.clients.length,
                   itemBuilder: (context, index) {
                     return ClientCard(
                       clientCardProps: ClientCardProps(
                         onTap: () => widget.onTap!(
-                          appData.filteredContacts[index],
+                          clientsMgr.clients[index],
                         ),
                         withNavigation: false,
-                        contactDetails: appData.filteredContacts[index],
+                        contactDetails: clientsMgr.clients[index],
                       ),
                     );
                   },
