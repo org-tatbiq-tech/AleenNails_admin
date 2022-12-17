1b04edc3c133fab33c92f1cd1d701b5a92ebd622
Ahmad <ahmadmnaa.b@gmail.com>
2022-12-17 12:04:00 +0200

    appointment details UI

--- lib/screens/home/clients/client_appointments.dart
+++ lib/screens/home/clients/client_appointments.dart
@@ -3,6 +3,7 @@ import 'package:appointments/providers/clients_mgr.dart';
 import 'package:appointments/screens/home/appointments/appointment.dart';
 import 'package:appointments/widget/appointment/client_appointment_card.dart';
 import 'package:common_widgets/custom_app_bar.dart';
+import 'package:common_widgets/custom_container.dart';
 import 'package:common_widgets/custom_text_button.dart';
 import 'package:common_widgets/empty_list_image.dart';
 import 'package:common_widgets/utils/general.dart';
@@ -25,84 +26,90 @@ class ClientAppointmentsState extends State<ClientAppointments> {
   Widget build(BuildContext context) {
     return Consumer<ClientsMgr>(
       builder: (context, clientsMgr, _) {
-        return Scaffold(
-          backgroundColor: Theme.of(context).colorScheme.background,
-          appBar: CustomAppBar(
-            customAppBarProps: CustomAppBarProps(
-              titleText:
-                  Languages.of(context)!.clientAppointmentsLabel.toTitleCase(),
-              withBack: true,
-              customIcon: Icon(
-                FontAwesomeIcons.plus,
-                size: rSize(20),
-              ),
-              customIconTap: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        AppointmentScreen(client: clientsMgr.selectedClient),
-                  ),
+        return CustomContainer(
+          imagePath: 'assets/images/background4.png',
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: CustomAppBar(
+              customAppBarProps: CustomAppBarProps(
+                titleText: Languages.of(context)!
+                    .clientAppointmentsLabel
+                    .toTitleCase(),
+                withBack: true,
+                isTransparent: true,
+                customIcon: Icon(
+                  FontAwesomeIcons.plus,
+                  size: rSize(20),
                 ),
-              },
+                customIconTap: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          AppointmentScreen(client: clientsMgr.selectedClient),
+                    ),
+                  ),
+                },
+              ),
             ),
-          ),
-          body: AnimatedSwitcher(
-            duration: const Duration(milliseconds: 500),
-            child: clientsMgr.selectedClient.appointments.isEmpty
-                ? EmptyListImage(
-                    emptyListImageProps: EmptyListImageProps(
-                      title: Languages.of(context)!
-                          .emptyAppointmentListLabel
-                          .toCapitalized(),
-                      iconPath: 'assets/icons/menu.png',
-                      bottomWidget: CustomTextButton(
-                        customTextButtonProps: CustomTextButtonProps(
-                          onTap: () => {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => AppointmentScreen(
-                                    client: clientsMgr.selectedClient),
+            body: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              child: clientsMgr.selectedClient.appointments.isEmpty
+                  ? EmptyListImage(
+                      emptyListImageProps: EmptyListImageProps(
+                        title: Languages.of(context)!
+                            .emptyAppointmentListLabel
+                            .toCapitalized(),
+                        iconPath: 'assets/icons/menu.png',
+                        bottomWidget: CustomTextButton(
+                          customTextButtonProps: CustomTextButtonProps(
+                            onTap: () => {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AppointmentScreen(
+                                      client: clientsMgr.selectedClient),
+                                ),
                               ),
+                            },
+                            text: Languages.of(context)!
+                                .addNewAppointmentLabel
+                                .toTitleCase(),
+                            textColor: Theme.of(context).colorScheme.primary,
+                            withIcon: true,
+                            icon: Icon(
+                              FontAwesomeIcons.plus,
+                              size: rSize(16),
+                              color: Theme.of(context).colorScheme.primary,
                             ),
-                          },
-                          text: Languages.of(context)!
-                              .addNewAppointmentLabel
-                              .toTitleCase(),
-                          textColor: Theme.of(context).colorScheme.primary,
-                          withIcon: true,
-                          icon: Icon(
-                            FontAwesomeIcons.plus,
-                            size: rSize(16),
-                            color: Theme.of(context).colorScheme.primary,
                           ),
                         ),
                       ),
+                    )
+                  : ListView.separated(
+                      padding: EdgeInsets.symmetric(
+                        vertical: rSize(40),
+                        horizontal: rSize(30),
+                      ),
+                      itemCount: clientsMgr.selectedClient.appointments.length,
+                      itemBuilder: (context, index) {
+                        return ClientAppointmentCard(
+                          clientAppointmentCardProps:
+                              ClientAppointmentCardProps(
+                            clientAppointmentDetails:
+                                clientsMgr.selectedClient.appointments[index],
+                            withNavigation: true,
+                            enabled: true,
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          height: rSize(15),
+                        );
+                      },
                     ),
-                  )
-                : ListView.separated(
-                    padding: EdgeInsets.symmetric(
-                      vertical: rSize(40),
-                      horizontal: rSize(30),
-                    ),
-                    itemCount: clientsMgr.selectedClient.appointments.length,
-                    itemBuilder: (context, index) {
-                      return ClientAppointmentCard(
-                        clientAppointmentCardProps: ClientAppointmentCardProps(
-                          clientAppointmentDetails:
-                              clientsMgr.selectedClient.appointments[index],
-                          withNavigation: true,
-                          enabled: true,
-                        ),
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) {
-                      return SizedBox(
-                        height: rSize(15),
-                      );
-                    },
-                  ),
+            ),
           ),
         );
       },
