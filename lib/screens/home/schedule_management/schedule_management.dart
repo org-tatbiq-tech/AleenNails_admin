6e568fc2920cfe7b61bdafaffdf149494c6c3934
S.Isa <s.isa.developments@gmail.com>
2022-12-24 23:10:31 +0200

    schedule management dispose issue

--- lib/screens/home/schedule_management/schedule_management.dart
+++ lib/screens/home/schedule_management/schedule_management.dart
@@ -1,8 +1,7 @@
 import 'package:appointments/localization/language/languages.dart';
 import 'package:appointments/providers/settings_mgr.dart';
-import 'package:common_widgets/custom_container.dart';
-
 import 'package:common_widgets/custom_app_bar.dart';
+import 'package:common_widgets/custom_container.dart';
 import 'package:common_widgets/custom_icon_button.dart';
 import 'package:common_widgets/fade_animation.dart';
 import 'package:common_widgets/utils/general.dart';
@@ -20,13 +19,6 @@ class ScheduleManagement extends StatefulWidget {
 }
 
 class ScheduleManagementState extends State<ScheduleManagement> {
-  @override
-  void dispose() {
-    SettingsMgr settingsMgr = Provider.of<SettingsMgr>(context, listen: false);
-    settingsMgr.pauseScheduleManagement();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return CustomContainer(
